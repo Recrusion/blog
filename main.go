c7d1955c0fbe0090f23723f220fa55862422ffd7
Recrusion <entertohacking@gmail.com>
2025-11-12 18:40:08 +0300

    changed main

--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/Recrusion/blog-api/internal/configs"
+	"github.com/Recrusion/blog-api/internal/loader"
 	"github.com/Recrusion/blog-api/internal/repository"
 )
 
 func main() {
-	cfg, err := configs.NewConfig(8080, 5432, "postgres", "postgres", "postgres", "postgres", "localhost")
+	env, err := loader.LoadFromEnv()
+	if err != nil {
+		log.Printf("error: %v", err)
+	}
+
+	serverPort, _ := strconv.Atoi(env.GetServerPort())
+	dbPort, _ := strconv.Atoi(env.GetDBPort())
+	cfg, err := configs.NewConfig(serverPort, dbPort, env.GetDBDriver(), env.GetDBName(), env.GetDBUsername(), env.GetDBPassword(), env.GetDBHost())
 	if err != nil {
 		log.Fatalf("config creation failed: %v", err)
 	}
