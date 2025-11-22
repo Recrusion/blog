5e056afa194bf0ba761fc52f9ae5711a442f080a
Recrusion <entertohacking@gmail.com>
2025-11-22 22:07:02 +0300

    added level logging

--- main.go
+++ main.go
@@ -13,27 +13,27 @@ func main() {
 	// Инициализация переменных окружения
 	env, err := loader.LoadFromEnv()
 	if err != nil {
-		log.Fatalf(".env undefined: %v", err)
+		log.Fatalf("[ERROR] .env undefined: %v", err)
 	}
 
 	// Инициализация конфига
 	cfg, err := configs.NewConfig(env.GetServerPort(), env.GetDBPort(), env.GetDBDriver(), env.GetDBName(), env.GetDBUsername(), env.GetDBPassword(), env.GetDBHost())
 	if err != nil {
-		log.Fatalf("config creation failed: %v", err)
+		log.Fatalf("[ERROR] config creation failed: %v", err)
 	}
 
-	log.Printf("config created successfully")
+	log.Printf("[INFO] config created successfully")
 
 	// Создание объекта базы данных
 	db, err := repository.ConnectDatabase(cfg.GetDatabaseConfig().GetDBDriver(), cfg.GetDatabaseConfig().GetDSN())
 	if err != nil {
-		log.Fatalf("failed connection to database: %v", err)
+		log.Fatalf("[ERROR] failed connection to database: %v", err)
 	}
 	defer db.Close()
-	log.Printf("connection to database successfully")
+	log.Printf("[INFO] connection to database successfully")
 
 	// Инициализация сервера
 	if err = http.ListenAndServe(cfg.GetServerConfig().GetPort(), nil); err != nil {
-		log.Fatalf("server is down: %v", err)
+		log.Fatalf("[ERROR] server is down: %v", err)
 	}
 }
