1a5c06ee0db79e67ed7e6bc8f14e6b1f6d749c50
Recrusion <entertohacking@gmail.com>
2025-11-23 21:29:32 +0300

    added create method

---
+++ internal/post/post.go
@@ -0,0 +1,26 @@
+package post
+
+import "time"
+
+// Структура поста
+type Post struct {
+	ID        int64     `json:"ID"`
+	Title     string    `json:"Title"`
+	Content   string    `json:"Content"`
+	Author    string    `json:"Author"`
+	CreatedAt time.Time `json:"CreatedAt"`
+	UpdatedAt time.Time `json:"UpdatedAt"`
+	Tags      []string  `json:"Tags"`
+}
+
+// Создание нового поста
+func (p *Post) NewPost(ID int64, title string, content string, tags []string) *Post {
+	return &Post{
+		ID:        ID,
+		Title:     title,
+		Content:   content,
+		Author:    p.Author,
+		CreatedAt: time.Now(),
+		Tags:      tags,
+	}
+}
---
+++ internal/repository/post.go
@@ -0,0 +1,16 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/Recrusion/blog-api/internal/post"
+)
+
+// Создание поста в базе данных
+func (d *Database) CreatePost(post *post.Post) error {
+	_, err := d.db.NamedExec("insert into post (id, title, content, author, createdAt, updatedAt, tags) values (:id, :title, :content, :author, :createdAt, :updatedAt, :tags)", post)
+	if err != nil {
+		return fmt.Errorf("error creating post: %w", err)
+	}
+	return nil
+}
