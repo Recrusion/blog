d0c6a5da5c797c1f44dae7bc572931730f0c9671
Recrusion <entertohacking@gmail.com>
2025-11-27 11:45:56 +0300

    fixed get post; added method for delete post

--- internal/repository/post.go
+++ internal/repository/post.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"database/sql"
 	"fmt"
 
 	"github.com/Recrusion/blog-api/internal/post"
@@ -16,10 +17,19 @@ func (d *Database) CreatePost(post *post.Post) error {
 }
 
 // получить пост по id
-func (d *Database) GetPost(id int64) error {
-	_, err := d.db.NamedExec("select * from post where id = :id", id)
+func (d *Database) GetPost(id int64) (*sql.Result, error) {
+	post, err := d.db.NamedExec("select * from post where id = :id", id)
 	if err != nil {
-		return fmt.Errorf("error getting post: %w", err)
+		return nil, fmt.Errorf("error getting post: %w", err)
+	}
+	return &post, nil
+}
+
+// удалить пост по id
+func (d *Database) DeletePost(id int64) error {
+	_, err := d.db.NamedExec("delete from post where id = :id", id)
+	if err != nil {
+		return fmt.Errorf("error deleting post: %w", err)
 	}
 	return nil
 }
