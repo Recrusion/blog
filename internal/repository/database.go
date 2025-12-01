5e16818a66024cd1af07c1c8e557188aed6fcebf
Recrusion <entertohacking@gmail.com>
2025-12-01 08:14:37 +0300

    added 'new db' func

--- internal/repository/database.go
+++ internal/repository/database.go
@@ -11,6 +11,12 @@ type Database struct {
 	db *sqlx.DB
 }
 
+func NewDatabase(db *sqlx.DB) *Database {
+	return &Database{
+		db: db,
+	}
+}
+
 // создание подключения к базе данных
 func ConnectDatabase(dbDriver, dsn string) (*sqlx.DB, error) {
 	db, err := sqlx.Connect(dbDriver, dsn)
