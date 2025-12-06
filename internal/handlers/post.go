06bb7fd37fbbe43c742c959ca7eac9821678eaa6
Recrusion <entertohacking@gmail.com>
2025-12-06 08:29:00 +0300

    added endpoint for delete post

--- internal/handlers/post.go
+++ internal/handlers/post.go
@@ -40,3 +40,18 @@ func (h *Handlers) GetPost(c echo.Context) error {
 
 	return c.JSON(http.StatusOK, post)
 }
+
+func (h *Handlers) DeletePost(c echo.Context) error {
+	idStr := c.Param("id")
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil {
+		c.Logger().Error(fmt.Errorf("Invalid request: %v", err))
+		return c.JSON(400, map[string]string{"error": "Invalid request"})
+	}
+	err = h.handler.DeletePost(id)
+	if err != nil {
+		c.Logger().Error(fmt.Errorf("Invalid request: %v", err))
+		return c.JSON(400, map[string]string{"error": "Invalid request"})
+	}
+	return c.JSON(http.StatusOK, map[string]string{"success": "Successfully deleted post with id " + strconv.FormatInt(id, 10)})
+}
